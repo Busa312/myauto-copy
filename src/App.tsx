bc88a2baed1d9816c8b47c3659668c37424f19b2
busa312 <besarionigadelia@gmail.com>
2023-05-27 21:57:39 +0400

    feature(main): remove testing code

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import React, {useState} from 'react';
 import './App.scss';
-import {getManufacturers} from "./services/httpCalls";
+import {getCategories, getManufacturers, getProducts} from "./services/httpCalls";
 
 function App() {
-
-  let [useMans, setMans] = useState([] as any[])
-    async function getData() {
-      setMans(await getManufacturers())
-    }
   return (
     <div className="App">
-      <button onClick={getData}>get data</button>
-      {useMans.map((m) => <p>{m?.man_name}</p>)}
     </div>
   );
 }
