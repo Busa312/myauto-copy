f0079682e69be3dc7c520f2d85a3b7301461ed7f
busa312 <besarionigadelia@gmail.com>
2023-05-27 21:54:59 +0400

    feature(main): add interfaces

--- src/services/httpCalls.ts
+++ src/services/httpCalls.ts
@@ -1,30 +1,37 @@
+import {ICategory, IManufacturer, IModel, IProduct} from "../interfaces/interfaces";
+
 const manufacturersURL = 'https://static.my.ge/myauto/js/mans.json';
 const modelsURL = 'https://api2.myauto.ge/ka/getManModels?man_id=';
 const catURL = 'https://api2.myauto.ge/ka/cats/get';
 const productsURL = 'https://api2.myauto.ge/ka/products/'
 
-export function getManufacturers() {
+// fetches data from api and returns array of manufacturers
+export function getManufacturers(): Promise<IManufacturer[]> {
     return fetch(manufacturersURL)
         .then((res) => res.json())
         .then((data) => {
-            return data;
+            return data as IManufacturer[]
         })
 }
 
-export function getModels(man_id: number) {
+// fetches data from api and returns array of models from manufacturer
+// @Param man_id manufacturer id
+export function getModels(man_id: number): Promise<IModel[]> {
     return fetch(modelsURL)
         .then(res => res.json())
-        .then(data => data);
+        .then(data => data.data as IModel[]);
 }
 
-export function getCategories() {
+// fetches data from api and returns array of categories
+export function getCategories() : Promise<ICategory[]> {
     return fetch(catURL)
         .then(res => res.json())
-        .then(data => data);
+        .then(data => data.data as ICategory[]);
 }
 
-export function getProducts() {
+// fetches data from api and returns array of products
+export function getProducts(): Promise<IProduct[]> {
     return fetch(productsURL)
         .then(res => res.json())
-        .then(data => data)
+        .then(data => data.data.items as IProduct[])
 }
