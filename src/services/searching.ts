95fe7a1aff8f64c2ff694223cf43e4c9d1878b51
busa312 <besarionigadelia@gmail.com>
2023-06-17 17:53:12 +0400

    price searching fix

--- src/services/searching.ts
+++ src/services/searching.ts
@@ -2,6 +2,7 @@ import {IProduct} from "../interfaces/interfaces";
 import {Period} from "../interfaces/types";
 import {periods} from "../interfaces/enums";
 import {sortBy} from "./sorting";
+import {State} from "./StateService";
 
 export function searchByRent(productList: IProduct[], forRent: boolean[], sorter: number = -1) {
     return sortBy(forRent.length> 0?productList.filter(val => val.for_rent === forRent[0]): productList, sorter);
@@ -16,11 +17,11 @@ export function searchByCategory(productList: IProduct[], categoryIdList: number
 }
 
 export function searchByPriceFrom(productList: IProduct[], priceFrom: number, sorter: number = -1) {
-    return sortBy(productList.filter(val => val.price >= priceFrom), sorter);
+    return sortBy(productList.filter(val =>State.store.isUsd? val.price >= priceFrom: val.price_usd >= priceFrom), sorter);
 }
 
 export function searchByPriceTo(productList: IProduct[], priceTo: number, sorter: number = -1) {
-    return sortBy(productList.filter(val => val.price <= priceTo), sorter);
+    return sortBy(productList.filter(val => State.store.isUsd? val.price <= priceTo: val.price_usd <= priceTo), sorter);
 }
 
 export function searchByPeriod(productsList: IProduct[], period: Period, sorter: number = -1){
